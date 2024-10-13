db99405f7a171b8cc3f6406593c75d401d7bd85f
Geuniv <kkoowl99@gmail.com>
2024-10-13 23:55:20 +0900

    2024.10.13 졸작 ( 네이버 로그인 API 작업중 )

--- src/main/java/kopo/poly/service/INaverAPIService.java
+++ src/main/java/kopo/poly/service/INaverAPIService.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 @FeignClient(name = "BookAPI", url = "https://openapi.naver.com")
 public interface INaverAPIService {
 
+    // 네이버 책 검색 API ( Open Feign ) - 2024.08.30
     @RequestLine("GET /v1/search/book.json?query={query}&display={display}&sort={sort}")
     BookDTO bookSearch(
             @Param("query") String query,
